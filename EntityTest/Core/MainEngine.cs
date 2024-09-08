72d9309eed8f4872727f961f7050211242626902
Brett Gilbert <kalain@kalain.co.uk>
2024-09-08 17:27:08 +0100

    Starting
    .

---
+++ EntityTest/Core/MainEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityTest.Core
+{
+    public static class MainEngine
+    {
+        public static void runEngine()
+        {
+
+        }
+    }
+}
---
+++ EntityTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EntityTest.Core;
+
+Console.WriteLine("Hello, World!");
+
+MainEngine.runEngine();
+
+
