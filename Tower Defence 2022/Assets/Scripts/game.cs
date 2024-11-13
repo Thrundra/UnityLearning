6f1dbf1c9b53951b60e71954e723593527de2e97
Brett Gilbert <kalain@kalain.co.uk>
2024-11-13 21:51:02 +0000

    tower defence
    Learn this you mong

---
+++ Tower Defence 2022/Assets/Scripts/game.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    public Transform prefab;
+    public KeyCode createkey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(createkey))
+        {
+            //Instantiate(prefab);
+            CreateObject();
+        }
+        else if(Input.GetKeyDown(newGameKey)
+        {
+            BeginNewGame();
+        }
+    }
+
+    void CreateObject ()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+    }
+
+    void BeginNewGame() { }
+}
