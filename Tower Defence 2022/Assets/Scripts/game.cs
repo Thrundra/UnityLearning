917179714d155a3387280f5585654aa5e5fb51ea
Brett Gilbert <Brett@kalain.co.uk>
2024-11-14 21:37:54 +0000

    Updating The Code
    Updating the code for saving data

--- Tower Defence 2022/Assets/Scripts/game.cs
+++ Tower Defence 2022/Assets/Scripts/game.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class game : MonoBehaviour
@@ -5,6 +7,16 @@ public class game : MonoBehaviour
     public Transform prefab;
     public KeyCode createkey = KeyCode.C;
     public KeyCode newGameKey = KeyCode.N;
+    public KeyCode saveKey = KeyCode.S;
+
+    List<Transform> objects;
+    string savePath;
+
+    void Awake()
+    {
+        objects = new List<Transform> ();
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,14 +26,18 @@ public class game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(createkey))
+        if (Input.GetKeyDown(createkey))
         {
             //Instantiate(prefab);
             CreateObject();
         }
-        else if(Input.GetKeyDown(newGameKey))
+        else if (Input.GetKeyDown(newGameKey))
         {
             BeginNewGame();
+        }
+        else if (Input.GetKey(saveKey))
+        {
+            Save();
         }
     }
 
@@ -31,7 +47,26 @@ public class game : MonoBehaviour
         t.localPosition = Random.insideUnitSphere * 5f;
         t.localRotation = Random.rotation;
         t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        objects.Add(t);
     }
 
-    void BeginNewGame() { }
+    void BeginNewGame() 
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    // Save and read data
+
+    void Save()
+    {
+        using (var writer = new BinaryWriter(File.Open(savePath, FileMode.Create)) )
+        {
+            writer.Write(objects.Count);
+        }
+  
+    }
 }
